a2f07f2213eaf2271c8b77fe1537cc1185c8e195
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2022-08-04 18:14:18 +0200

    ex.3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,38 +4,35 @@ const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
 
-const formData = {};
+
 const STORAGE_KEY = 'feedback-form-state';
 
-form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onTextareaInput, 500));
+form.addEventListener(
+    'input', throttle((event) => {
+      const objInput = {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+      };
+      localStorage.setItem('feedback-form-state', JSON.stringify(objInput));
+    }, 500)
+  );
 
-populateTextarea();
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const { elements: { email, message } } = event.currentTarget;
+    console.log({ email: email.value, message: message.value });
+    
+    event.currentTarget.reset();
+    localStorage.clear();
+});
 
 
-function onFormSubmit(event) {
-  event.preventDefault();
-  if (input.value === '' || textarea.value === '') {
-    return alert('Please, fill in form fields and push the button «Submit»');
+const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+const fillForm = () => {
+  if (savedMessage) {
+    input.value = parseStorage.email;
+    textArea.value = parseStorage.message;
   }
-  console.log(formData);
-  event.target.reset();
-  localStorage.removeItem(STORAGE_KEY);
-}
-
-function onTextareaInput(event) {
-  formData[input.name] = input.value;
-  formData[textarea.name] = textarea.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
-
-
-function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
-  const parsedMessage = JSON.parse(savedMessage);
-  if (!parsedMessage) {
-    return;
-  }
-  input.value = parsedMessage.email;
-  textarea.value = parsedMessage.message;
-}
+};
+fillForm();
