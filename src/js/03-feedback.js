747a02532a245a4f5732d917bd9ad499bfd1da99
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2022-08-04 17:45:23 +0200

    t3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,21 @@
 import throttle from 'lodash.throttle';
 
-const formEl = document.querySelector('.feedback-form');
-const mailEl = document.querySelector('[name="email"]');
-const messageEl = document.querySelector('[name="message"]');
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
 
 const formData = {};
 const STORAGE_KEY = 'feedback-form-state';
 
-formEl.addEventListener('submit', onFormSubmit);
-formEl.addEventListener('input', throttle(onTextareaInput, 500));
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextareaInput, 500));
 
 populateTextarea();
 
 
 function onFormSubmit(event) {
   event.preventDefault();
-  if (mailEl.value === '' || messageEl.value === '') {
+  if (input.value === '' || textarea.value === '') {
     return alert('Please, fill in form fields and push the button «Submit»');
   }
   console.log(formData);
@@ -24,8 +24,8 @@ function onFormSubmit(event) {
 }
 
 function onTextareaInput(event) {
-  formData[mailEl.name] = mailEl.value;
-  formData[messageEl.name] = messageEl.value;
+  formData[input.name] = input.value;
+  formData[textarea.name] = textarea.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
@@ -36,6 +36,6 @@ function populateTextarea() {
   if (!parsedMessage) {
     return;
   }
-  mailEl.value = parsedMessage.email;
-  messageEl.value = parsedMessage.message;
+  input.value = parsedMessage.email;
+  textarea.value = parsedMessage.message;
 }
